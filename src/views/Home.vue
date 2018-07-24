d6566be93507e54a7f3859dc73645a27e2c1b040
tally <tallyaffek@gmail.com>
2018-07-24 20:34:29 +0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Helloooooo</h1>
+      <h1>Hellooookjdfhdsfhsdkfsdjkfsdjkfsdjfhsdjkfhskfhdskfhkdsjfhkjsfsoo2</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
