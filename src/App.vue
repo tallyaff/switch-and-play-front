f6895c2ff79010a278c6add64b088413279b310d
Liron Reshef Steinberger <reshef.liron@gmail.com>
2018-07-25 21:48:54 +0300

    starting scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <img src="img/logo.png">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/contact">Contact</router-link>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './stores/store.js'
 import './registerServiceWorker'
-import   './assets/scss/style.scss'
+import './assets/scss/style.scss'
 Vue.config.productionTip = false
 
 new Vue({
