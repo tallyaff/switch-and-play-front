e4e0bba79f4dc67f53c40d9c3bcf0027cc050b09
tally <tallyaffek@gmail.com>
2018-07-26 09:20:13 +0300

    pull from wendsday

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Element from 'element-ui'
 import router from './router'
 import store from './stores/store.js'
+import 'element-ui/lib/theme-chalk/index.css';
 import './registerServiceWorker'
 import   './assets/scss/style.scss'
 Vue.config.productionTip = false
+Vue.use(Element)
 
 new Vue({
   router,
