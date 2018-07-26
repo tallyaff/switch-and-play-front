47cbfe492b026441f1a26e04365d174a6fae1e5e
Liron Reshef Steinberger <reshef.liron@gmail.com>
2018-07-26 17:42:07 +0300

    changes css in gallery

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -5,7 +5,7 @@
             <ul v-if="games">
             <!-- <li v-if="currToys" v-for="(toy ,idx) in currToys" :key="toy._id">
                 {{idx}} -->
-                <li class="game" v-for="game in games" :key="game._id">
+                <li class="game clean-list" v-for="game in games" :key="game._id">
                 <!-- <li class="masonry" v-if="games" v-for="game in games" :key="game._id"> -->
                     <game-preview :game="game" @remove="$emit('remove', game._id)"></game-preview>
                     <!-- {{toy.name}} -->
@@ -40,6 +40,8 @@ export default {
       width: 400px;
       border: 1px solid $border-color;  
       box-shadow: 0px 2px 4px 0px #d9d8d8;
+      border-radius: 20px;
+    
   }
 
 
--- src/components/GamePreview.vue
+++ src/components/GamePreview.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="game-preview" v-if="game">
-        <ul class="game-preview-container">
+  <!-- <div class="game-preview" > -->
+        <ul class="game-preview-container flex column align-center clean-list" v-if="game">
             <!-- :src="game.src"  -->
             <li class="game-name-preview">{{game.name}}</li>
-            <img :src="game.src" :alt="'no image'"/>
+            <div class="image-container" :style="{backgroundImage: url, backgroundSize: 'cover', backgroundPosition: 'center', backgroundrepeat: 'no-repeat'}">
+              <!-- this url{{url}} -->
+            <!-- <div class="image-container" :style="{backgroundImage: 'url('game.src}"> -->
+              <!-- <img :src="game.src" :alt="'no image'"/> -->
+            </div>
             <li class="game-category-preview">Category: {{game.category}}</li>
             <li class="game-condition-preview">Condition: {{game.condition}}</li>
             <li class="game-addedAt-preview">Added at: {{game.addedAt}}</li>
@@ -14,13 +18,17 @@
                 <router-link tag="button" :to="'/game/'+game._id">More details</router-link>
             </li>
         </ul>
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
   name: "GamePreview",
   props: ["game"],
+  data() {
+    return {}
+    // url("../../media/examples/lizard.png");
+  },
 
   methods: {
 
@@ -30,6 +38,9 @@ export default {
     //   // console.log('user!!!!!!!!!!!!!!!!!!', this.$store.getters.setLoginUser);
     //   return this.$store.getters.setLoginUser;
     // },
+    url() {
+      return `url("${this.game.src}")`;
+    } 
  
   }
 };
@@ -40,6 +51,30 @@ export default {
     .game-name-preview {
       font-size: rem(30px);
       font-family: 'Ubuntu';
+    }
+    .game-name-preview {
+      padding: rem(10px);
+
+
+    }
+    .image-container {
+      width: 100%;
+      height: 500px;
+    }
+      img {
+      // height: 50%;
+      width: 100%;
+      // background-repeat: no-repeat;
+
+    // width: 400px;
+    // height: 500px;
+    // margin: 10px;
+    // background-size: cover;
+    // background-position: center;
+    // background-repeat: no-repeat;
+    cursor: pointer;
+    transition: all 0.5s;
+    
   }
 
 
